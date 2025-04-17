e0dca8846b35e4a4df0e948246d53a08c5fb186c
dima <safonovdima330>
2025-04-17 23:31:30 +0300

    tests

--- src/test/java/com/bank/BankApplicationTests.java
+++ src/test/java/com/bank/BankApplicationTests.java
@@ -3,7 +3,10 @@ package com.bank;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.profiles.active=test",
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration"
+})
 class BankApplicationTests {
     @Test
     void contextLoads() {

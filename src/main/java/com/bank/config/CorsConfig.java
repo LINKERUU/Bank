3423b7a1f2997a80a53d5f51c9ad551e82d20272
dima <safonovdima330>
2025-05-30 01:10:27 +0300

    update final

--- src/main/java/com/bank/config/CorsConfig.java
+++ src/main/java/com/bank/config/CorsConfig.java
@@ -12,17 +12,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-  // Инжектируем значение переменной среды FRONTEND_URL
-  // Если переменная не установлена, будет использоваться значение по умолчанию 'http://localhost:3000'
-  // (замените на порт вашего фронтенда для локальной разработки)
-  @Value("${FRONTEND_URL}")
-  private String frontendUrl;
-
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
             // Используем инжектированное значение
-            .allowedOrigins(frontendUrl)
+            .allowedOrigins("https://frontend-for-bank-production.up.railway.app")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("Content-Type", "Authorization")
             .allowCredentials(true);
